9e6bee209637a4739dca9488d27ca2541a480a3a
labettle <143569940+labettle@users.noreply.github.com>
2025-08-11 20:52:17 +0800

    Delete td_project directory

--- td_project/.lock.f
+++
@@ -1,4 +0,0 @@
-DESKT-CNBDGRPWD
-admin
-10900
-D:\APP\Anlogic\fpga_project\I_Anlogic_lvds_7_1_P_N_dynamic_enc_double channel\td_project\.lock.f
--- td_project/LVDS_7_1_Runs/syn_1/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem Tcl script created by Tang Dynasty 6.1.2 154205
-rem Copyright (c) 2012-2025 Anlogic Inc.
-
-set HD_SDIR=%~dp0
-cd /d "%HD_SDIR%"
-D:/APP/Anlogic/bin/td_commands_prompt.exe D:/APP/Anlogic/arch/../doc/scripts/DefaultFlow.tcl
-pause
